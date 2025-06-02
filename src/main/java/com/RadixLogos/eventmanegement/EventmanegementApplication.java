3ddca63207214733854bc1f7dff596ed78b445db
rubens <rubenshds@gmail.com>
2025-06-02 19:42:40 -0300

    Categoria

---
+++ src/main/java/com/RadixLogos/eventmanegement/EventmanegementApplication.java
@@ -0,0 +1,13 @@
+package com.RadixLogos.eventmanegement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmanegementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventmanegementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RadixLogos/eventmanegement/EventmanegementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RadixLogos.eventmanegement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmanegementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
